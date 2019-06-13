6d8f317f53f28935c3e2b8906ca11323f8f3b038
jpavioli <justin.avioli@gmail.com>
2019-06-13 16:11:54 -0500

    Built out API pull to Seed Players and Stats DB

--- fantasy-baseball/backend/seeds.js
+++ fantasy-baseball/backend/seeds.js
@@ -5,6 +5,15 @@ const FantasyTeams = require('./models/FantasyTeams')
 const Leagues = require('./models/Leagues')
 const Users = require('./models/users')
 
+const d = new Date ()
+const BASE_URL = 'http://lookup-service-prod.mlb.com'
+const TEAMS_URL = `${BASE_URL}/json/named.team_all_season.bam?sport_code='mlb'&all_star_sw='N'&sort_order=name_asc&season='${d.getFullYear()}'`
+const PLAYER_ROSTER_URL = (teamID) => `${BASE_URL}/json/named.roster_40.bam?team_id='${teamID}'`
+const CAREER_HITTING_STATS_URL = (playerID) => `${BASE_URL}/json/named.sport_career_hitting.bam?league_list_id='mlb'&game_type='R'&player_id='${playerID}'`
+const CAREER_PITCHING_STATS_URL = (playerID) => `${BASE_URL}/json/named.sport_career_pitching.bam?league_list_id='mlb'&game_type='R'&player_id='${playerID}'`
+const SEASON_HITTING_STATS_URL = (playerID) => `${BASE_URL}/json/named.sport_hitting_tm.bam?league_list_id='mlb'&game_type='R'&season='${d.getFullYear()}'&player_id='${playerID}'`
+const SEASON_PITCHING_STATS_URL = (playerID) => `${BASE_URL}/json/named.sport_pitching_tm.bam?league_list_id='mlb'&game_type='R'&season='${d.getFullYear()}'&player_id='${playerID}'`
+
 Stats.sync()
 Players.sync()
 FantasyTeams.sync()
@@ -12,44 +21,103 @@ Leagues.sync()
 Users.sync()
 
 //create seed data for Stats
-const stats = [
-    {
-        "playerId": 493316,
-        "career": false,
-        "start": "2017-12-12T00:00:00",
-        "end": "2017-01-01T00:00:00",
-        "h": 85, //singles
-        "d": 17, //dobules
-        "t": 2, //triples
-        "hr": 17, //homeruns
-        "r": 46, //runs
-        "rbi": 42, //RBI
-        "bb": 26, //Walks
-        "sb": 0, //StolenBase
-        "cs": 1, //Caught Stealing
-        "avg": 0.292, //batting average
-        "p_w": 1, //Pitcher - Wins
-        "p_k": 34, //Pitcher - strikeouts (so)
-        "p_era": 10, //Pitcher - Earned Runs Allowed (er)
-        "p_hwa": 32//Pitcher - Hits + Walks Allowed (bb + h)
-    },
-]
-stats.forEach(stat => Stats.create(stat))
-
-//create seed data for players
-const players = [
-    {
-        "id": 493316,
-        "fantasyTeamId": 1,
-        "team_name":"New York Mets",
-        "name_display_first_last": "Yoenis Cespedes",
-        "primary_position_txt":"LF",
-        "age": 32,
-        "jersey_number":52,
-        "primary_stat_type":"hitting",
-    },
-]
-players.forEach(player => Players.create(player))
+fetch(TEAMS_URL)
+  .then(res=>res.json())
+  .then(data=>{
+    let teamIDs = data.team_all_season.queryResults.row.map(team => team.team_id)
+    teamIDs.map((teamID)=> {
+      fetch(PLAYER_ROSTER_URL(teamID))
+        .then(res=>res.json())
+        .then(data=>{
+          let players = data.roster_40.queryResults.row
+          players.map((player)=>{
+            let playerOBJ = {
+              id: player.player_id,
+              fantasyTeamId: null,
+              team_name: player.team_name,
+              name_display_first_last: player.name_display_first_last,
+              primary_position_txt: player.position_txt,
+              age: d.getFullYear() - parseInt(player.birth_date.slice(0,4)),
+              jersey_number: player.jersey_number,
+              primary_stat_type: player.position_txt === "P" ? "pitching" : "hitting"
+            }
+            Players.create(playerOBJ)
+            if (player.position_txt === "P") {
+              fetch(CAREER_PITCHING_STATS_URL(player.player_id))
+                .then(res=>res.json())
+                .then(data=>{
+                  let careerStats = data.sport_career_pitching.queryResults.row
+                  let statOBJ = {
+                      playerId: careerStats.player_id,
+                      career: true,
+                      p_w: careerStats.bb,
+                      p_k: careerStats.so,
+                      p_era: careerStats.era,
+                      p_hwa: parseInt(careerStats.h) + parseInt(careerStats.bb)
+                    }
+                  Stats.create(statOBJ)
+                })
+                fetch(SEASON_PITCHING_STATS_URL(player.player_id))
+                  .then(res=>res.json())
+                  .then(data=>{
+                    let careerStats = data.sport_career_pitching.queryResults.row
+                    let statOBJ = {
+                        playerId: careerStats.player_id,
+                        career: false,
+                        p_w: careerStats.bb,
+                        p_k: careerStats.so,
+                        p_era: careerStats.era,
+                        p_hwa: parseInt(careerStats.h) + parseInt(careerStats.bb)
+                      }
+                    Stats.create(statOBJ)
+                  })
+            }
+            else {
+              fetch(CAREER_HITTING_STATS_URL(player.player_id))
+                .then(res=>res.json())
+                .then(data=>{
+                  let careerStats = data.sport_career_hitting.queryResults.row
+                  let statOBJ = {
+                      playerId: careerStats.player_id,
+                      career: true,
+                      h: careerStats.h,
+                      d: careerStats.d,
+                      t: careerStats.t,
+                      hr: careerStats.hr,
+                      r: careerStats.r,
+                      rbi: careerStats.rbi,
+                      bb: careerStats.bb,
+                      sb: careerStats.sb,
+                      cs: careerStats.cs,
+                      avg: careerStats.avg
+                    }
+                  Stats.create(statOBJ)
+                })
+              fetch(SEASON_HITTING_STATS_URL(player.player_id))
+                .then(res=>res.json())
+                .then(data=>{
+                  let careerStats = data.sport_career_hitting.queryResults.row
+                  let statOBJ = {
+                      playerId: careerStats.player_id,
+                      career: false,
+                      h: careerStats.h,
+                      d: careerStats.d,
+                      t: careerStats.t,
+                      hr: careerStats.hr,
+                      r: careerStats.r,
+                      rbi: careerStats.rbi,
+                      bb: careerStats.bb,
+                      sb: careerStats.sb,
+                      cs: careerStats.cs,
+                      avg: careerStats.avg
+                    }
+                  Stats.create(statOBJ)
+                })
+            }
+          })
+        })
+    })
+  })
 
 //create seed data for Fantasy Teams
 const fantasyTeam = [

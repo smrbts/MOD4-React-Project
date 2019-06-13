2e47814cd4cad1bfeea7c4e0187ef1bba892b7ac
jpavioli <justin.avioli@gmail.com>
2019-06-13 14:22:16 -0500

    Server is working for all DBs

---
+++ fantasy-baseball/backend/models/FantasyTeams.js
@@ -0,0 +1,40 @@
+const Sequelize = require('sequelize')
+
+const STRING = Sequelize.STRING
+const INTEGER = Sequelize.INTEGER
+const BOOLEAN = Sequelize.BOOLEAN
+// const (STRING) = Sequelize (Samething but different syntax)
+
+//Open Database Connection
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './database.sqlite'
+})
+
+//Create the Schema
+const FantasyTeams = sequelize.define('fantasyTeams',{
+    id:{
+      type: INTEGER,
+      primaryKey: true
+    },
+    userId: {type: INTEGER},
+    leagueId: {type: INTEGER},
+    teamName: {type: STRING},
+    imgURL: {type: STRING},
+    pitcher: {type: INTEGER},
+    catcher: {type: INTEGER},
+    firstBase: {type: INTEGER},
+    secondBase: {type: INTEGER},
+    thirdBase: {type: INTEGER},
+    shortStop: {type: INTEGER},
+    leftField: {type: INTEGER},
+    centerField: {type: INTEGER},
+    rightField: {type: INTEGER},
+    bench1: {type: INTEGER},
+    bench2: {type: INTEGER},
+    bench3: {type: INTEGER}
+})
+
+module.exports = FantasyTeams
+
+sequelize.sync()
--- fantasy-baseball/backend/seeds.js
+++ fantasy-baseball/backend/seeds.js
@@ -1,15 +1,15 @@
 //require the model file
 const Stats = require('./models/Stats')
 const Players = require('./models/Players')
-// const FantasyTeams = require('./models/fantasy-team')
-// const League = require('./models/league')
-// const Users = require('./models/users')
+const FantasyTeams = require('./models/FantasyTeams')
+const Leagues = require('./models/Leagues')
+const Users = require('./models/users')
 
 Stats.sync()
 Players.sync()
-// FantasyTeams.sync()
-// League.sync()
-// Users.sync()
+FantasyTeams.sync()
+Leagues.sync()
+Users.sync()
 
 //create seed data for Stats
 const stats = [
@@ -40,6 +40,7 @@ stats.forEach(stat => Stats.create(stat))
 const players = [
     {
         "id": 493316,
+        "fantasyTeamId": 1,
         "team_name":"New York Mets",
         "name_display_first_last": "Yoenis Cespedes",
         "primary_position_txt":"LF",
@@ -50,5 +51,51 @@ const players = [
 ]
 players.forEach(player => Players.create(player))
 
+//create seed data for Fantasy Teams
+const fantasyTeam = [
+    {
+        "id": 1,
+        "userId": 1,
+        "leagueId": 1,
+        "teamName": "The Houston Hashtronauts",
+        "imgURL": "https://spacecenter.org/wp-content/uploads/2013/04/astronaut_main_bg.jpg",
+        "pitcher": null,
+        "catcher": null,
+        "firstBase": null,
+        "secondBase": null,
+        "thirdBase": null,
+        "shortStop": null,
+        "leftField": 493316,
+        "centerField": null,
+        "rightField": null,
+        "bench1": null,
+        "bench1": null,
+        "bench3": null
+    }
+]
+fantasyTeam.forEach(team => FantasyTeams.create(team))
+
+const leagues = [
+    {
+        "id": 1,
+        "userId": 1,
+        "leagueName": "Default League",
+        "imageURL": "https://a2.espncdn.com/combiner/i?img=%2Fi%2Fespn%2Fmisc_logos%2F500%2Fflb.png",
+        "description": "This is a League for Winners. Clearly."
+    }
+]
+leagues.forEach(team => Leagues.create(team))
+
+const users = [
+    {
+        "id": 1,
+        "username": "SamTheMan",
+        "password": "FluffyDuck",
+        "first_name": "Sam",
+        "last_name": "Man",
+        "bio": "My middle name is 'The'"
+    }
+]
+users.forEach(user => Users.create(user))
 
 //seed data using 'node seeds.js'

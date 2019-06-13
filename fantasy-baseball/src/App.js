3c2071bd3e92ebe3788c8fbfdbd2cd3860939e65
smrbts <samueljustinroberts@gmail.com>
2019-06-13 16:09:01 -0500

    starting out

--- fantasy-baseball/src/App.js
+++ fantasy-baseball/src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component}from 'react';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import WelcomePage from './components/WelcomePage'
+import Home from './components/Home'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component 
+{
+  render()
+  {
+    return (
+      <BrowserRouter>
+       <div>
+         <Switch>
+          <Route exact path='/' render={(routeProps) => <WelcomePage {...routeProps} /> }/>
+          <Route path='/home' render={(routeProps) => <Home {...routeProps} />} />
+         </Switch>
+       </div>
+      </BrowserRouter>
+    );
+  }
+ 
 }
-
-export default App;
---
+++ fantasy-baseball/src/components/WelcomePage.js
@@ -0,0 +1,14 @@
+import React, {Component}from 'react';
+
+export default class WelcomePage extends Component 
+{
+  render()
+  {
+    return (
+      <div>
+          Sign In Page
+      </div>
+    );
+  }
+ 
+}
